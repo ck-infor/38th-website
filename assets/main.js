78541a0012e9c12fd29ea1b78e3cb1b002718f4a
JasonChen980330 <jasonellery0330@gmail.com>
2025-08-19 23:11:19 +0800

    added course

--- assets/main.js
+++ assets/main.js
@@ -12,4 +12,29 @@ imagesLoaded(elem, function(){
   });
 });
 
+// 課程介紹那的swiper
+var swiper = new Swiper(".mySwiper", {
+  // effect: 'coverflow',
+  // slidesPerView: 1.5,
+  // loop: true,
+  // centeredSlides: true,
+  effect: 'cards',
+  // slidesPerView: 1,
+  coverflowEffect: {
+      // depth: 200,
+      // rotate: 0,
+      // stretch: 0,
+      rotate: 20,
+      slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 
